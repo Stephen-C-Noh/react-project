b8075bb17b6cb7571a1017c53ee13306a4116358
Stephen-C-Noh <stephen.c.noh@gmail.com>
2022-03-30 16:35:33 -0400

    3.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+    const [value, setValue] = useState("");
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        //console.log(event.currentTarget.value); // in Typescripts, you use currentTarget instead of target in JS.
+        const {
+            currentTarget: { value },
+        } = event;
+        setValue(value);
+        console.log(value);
+    };
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("Hello, ", value);
+    };
     return (
         <div>
-            <Circle borderColor="blue" bgColor="teal" />
-            <Circle text="text is here" bgColor="tomato" />
+            <form onSubmit={onSubmit}>
+                <input value={value} type="text" placeholder="value" onChange={onChange} />
+                <button>Log in</button>
+            </form>
         </div>
     );
 }
