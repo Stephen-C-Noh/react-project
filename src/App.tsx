90e63cc9e7e11d5e77128ffc46b8ab67253047c9
Stephen-C-Noh <stephen.c.noh@gmail.com>
2022-03-30 17:18:29 -0400

    3.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+// When you import a package not made in Typescript(which means it does not have typescript declaration),
+// A)
+// try: npm i --save-dev @types/PACKAGENAME
+// for example npm -i --save-dev @types/react-query
+// B)
+// look up the package in DefinitelyTyped repository on Github.
+// the packages on DefinitelyTyped are explainations for Typescripts to understand what you have imported.
+
 import React, { useState } from "react";
 import styled from "styled-components";
 
