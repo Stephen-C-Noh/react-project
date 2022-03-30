667bd75c9b19761e3f2234c1595a069e4dc62817
Stephen-C-Noh <stephen.c.noh@gmail.com>
2022-03-30 06:29:29 -0400

    3.3

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+    return (
+        <div>
+            <Circle borderColor="blue" bgColor="teal" />
+            <Circle text="text is here" bgColor="tomato" />
+        </div>
+    );
+}
+
+export default App;
