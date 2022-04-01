5e3fd205a62aa1ed12463dd128beb548c146fd4a
Stephen-C-Noh <stephen.c.noh@gmail.com>
2022-04-01 13:07:00 -0400

    4.12 fetching coin history

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -195,10 +195,10 @@ function Coin() {
 
                     <Switch>
                         <Route path={`/${coinId}/price`}>
-                            <Price />
+                            <Price coinId={coinId} />
                         </Route>
                         <Route path={`/${coinId}/chart`}>
-                            <Chart />
+                            <Chart coinId={coinId} />
                         </Route>
                     </Switch>
                 </>
